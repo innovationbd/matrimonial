5b7d6321aedceff0a565456040cc51825da1bddd
biltechsoft <64979303+biltechsoft@users.noreply.github.com>
2021-10-23 22:47:47 +0600

    dep

--- src/app/examples/examples.module.ts
+++ src/app/examples/examples.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { LandingComponent } from './landing/landing.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -13,6 +14,7 @@ import { LoginComponent } from './login/login.component';
         CommonModule,
         FormsModule,
         NgbModule,
+        RouterModule
     ],
     declarations: [
         LandingComponent,
--- src/app/examples/signup/signup.component.ts
+++ src/app/examples/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-signup',
@@ -9,7 +10,17 @@ export class SignupComponent implements OnInit {
     test : Date = new Date();
     focus;
     focus1;
+    date: {year: number, month: number};
+    model: NgbDateStruct;
     constructor() { }
+    isWeekend(date: NgbDateStruct) {
+        const d = new Date(date.year, date.month - 1, date.day);
+        return d.getDay() === 0 || d.getDay() === 6;
+    }
+
+    isDisabled(date: NgbDateStruct, current: {month: number}) {
+        return date.month !== current.month;
+    }
 
     ngOnInit() {}
 }
